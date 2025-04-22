5362f737eca3655b5efb655089488dac9156bbbf
SulthonNashir <nashir@zetpy.com>
2025-04-22 18:06:52 +0700

    feat: param

--- app/quizzes/[id]/page.tsx
+++ app/quizzes/[id]/page.tsx
@@ -8,17 +8,18 @@ import LeaderboardTable from "@/app/components/LeaderboardTable";
 
 export const revalidate = 0;
 
-export default async function QuizDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function QuizDetailPage(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
   const id = params.id;
-  
+
   if (!id) {
     return notFound();
   }
-  
+
   const supabase = await createClient();
 
   // Fetch quiz details
--- app/quizzes/[id]/questions/page.tsx
+++ app/quizzes/[id]/questions/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import QuestionsEditor from "@/app/quizzes/[id]/questions/QuestionsEditor";
 
-export default async function QuestionsPage({params}: {params: { id: string }}) {
+export default async function QuestionsPage(props: {params: Promise<{ id: string }>}) {
+  const params = await props.params;
   const id = params.id;
-  
+
   return (
     <Suspense fallback={<div className="p-8 text-center">Loading...</div>}>
       <QuestionsEditor quizId={id} />
