f997f5575155fab880f71fc91d485b1f1d15274d
SulthonNashir <nashir@zetpy.com>
2025-04-23 16:55:39 +0700

    feat: add delete quiz and total done

--- app/brainbee/layout.tsx
+++ app/brainbee/layout.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import PasswordProtect from "@/components/PasswordProtect";
 import { usePathname } from "next/navigation";
+
+function BrainbeeLayoutClient({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  
+  // Don't password protect these paths
+  if (pathname.startsWith('/brainbee/passcode') || pathname.startsWith('/brainbee/session')) {
+    return <>{children}</>;
+  }
+  
+  // Password protect all other paths under /brainbee
+  return <PasswordProtect>{children}</PasswordProtect>;
+}
 
 export default function BrainbeeLayout({
   children,
@@ -12,18 +26,4 @@ export default function BrainbeeLayout({
       {children}
     </BrainbeeLayoutClient>
   );
-}
-
-"use client";
-
-function BrainbeeLayoutClient({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  
-  // Don't password protect these paths
-  if (pathname.startsWith('/brainbee/passcode') || pathname.startsWith('/brainbee/session')) {
-    return <>{children}</>;
-  }
-  
-  // Password protect all other paths under /brainbee
-  return <PasswordProtect>{children}</PasswordProtect>;
 } 
